de919db82fb2e99f79afddfdd3eb29c37de401dd
jaceuk <jnewington@gmail.com>
2022-08-20 15:22:52 +0100

    Added google icon

--- components/SignIn/SignIn.tsx
+++ components/SignIn/SignIn.tsx
@@ -1,5 +1,6 @@
 import Button from '@components/Button';
 import { signIn } from 'next-auth/react';
+import { GoogleCircled } from 'iconoir-react';
 import styles from './SignIn.module.scss';
 
 export default function SignIn() {
@@ -14,7 +15,10 @@ export default function SignIn() {
       </main>
       <div className={styles.buttons}>
         <button onClick={() => signIn('google')}>
-          <Button active>Sign in with Google</Button>
+          <Button active>
+            <GoogleCircled />
+            Sign in with Google
+          </Button>
         </button>
       </div>
     </div>
