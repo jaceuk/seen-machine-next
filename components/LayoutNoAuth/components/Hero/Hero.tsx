859882a90ef2dbb8597f9fe4e35711bd09214a71
jaceuk <jnewington@gmail.com>
2022-08-26 15:34:10 +0100

    Reworked scrolling

--- components/LayoutNoAuth/components/Hero/Hero.tsx
+++ components/LayoutNoAuth/components/Hero/Hero.tsx
@@ -4,11 +4,13 @@ import heroPlaceholder from '/images/hero.jpg';
 
 export default function Hero() {
   return (
-    <div className={styles.hero}>
-      <Image layout="fill" objectFit="cover" priority placeholder="blur" src={heroPlaceholder} alt="" />
+    <div className={styles.container}>
+      <div className={styles.hero}>
+        <Image layout="fill" objectFit="cover" priority placeholder="blur" src={heroPlaceholder} alt="" />
 
-      <div className={styles.footer}>
-        <div className={styles.fadeOut}></div>
+        <div className={styles.footer}>
+          <div className={styles.fadeOut}></div>
+        </div>
       </div>
     </div>
   );
